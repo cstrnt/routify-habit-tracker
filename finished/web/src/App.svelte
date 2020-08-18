02a1978b85357e39fbe01207306ec39d886e3ded
Tim Raderschad <tim.raderschad@gmail.com>
2020-08-18 16:46:09 +0200

    feat: add frontend

---
+++ finished/web/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Router } from "@sveltech/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style global>
+  @import "../static/global.css";
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<Router {routes} />
